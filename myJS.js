6d89c348567dbaed52e6ec8b4ee483eeac55f6be
sreevatsan1991 <sreevatsan1991@hotmail.com>
2017-08-30 01:19:56 -0400

    Add files via upload

---
+++ myJS.js
@@ -0,0 +1,10 @@
+var myPlot = document.getElementById('map');
+
+myPlot.on('plotly_click', function(data){
+    var pts = '';
+    for(var i=0; i < data.points.length; i++){
+        pts = 'x = '+data.points[i].x +'\ny = '+
+            data.points[i].y.toPrecision(4) + '\n\n';
+    }
+    alert('Closest point clicked:\n\n'+pts);
+});
